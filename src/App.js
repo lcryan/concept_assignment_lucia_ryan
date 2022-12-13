e0fc03805c1c5c187ee202b873c80b03e89250fe
LuciaRyan <lcryan06@gmail.com>
2022-12-13 17:06:51 +0100

    routing started - has to be finalized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import './index.css'
 import './App.css'
 import Logo from './assets/logo.png'
+import {BrowserRouter as Router} from "react-router-dom";
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
     return (
 
-        <>
-            <header className="app-header">
 
+        <>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+
+            </Routes>
+
+
+            <header className="app-header">
                 <div className="inner-container">
-                    <nav>
+                    <nav className="navigation-bar">
                         <ul>
-                            <li>nav item 1</li>
+                            <li>
+                                <uppercase>hottest spots</uppercase>
+                            </li>
                             <li>nav item 1</li>
                             <li>nav item 1</li>
                         </ul>
-
-                        <img src={Logo} id="logo-image" alt='reddit-logo'/>
-
-                        <h1>Reddit</h1>
                     </nav>
+
+                    <img src={Logo} id="logo-image" alt='reddit-logo'/>
+                    <h1>Reddit</h1>
+
                 </div>
 
 
