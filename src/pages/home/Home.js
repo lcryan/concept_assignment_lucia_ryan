646d5dcd852e75efffa26e95230f501817f18133
LuciaRyan <lcryan06@gmail.com>
2022-12-16 12:33:09 +0100

    Home.js mapped through array and fetched title and id data for now : functional, more to get and pack into p and so forth. deleted Navigation.js which I had falsely imported before: is on all pages, thus in component and in App.js deleted subreddit props from function Home - didn't have any effect and was falsely injected.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,41 +1,61 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Logo from "../../assets/logo.png";
-import {useEffect, useState} from "react";
 import axios from "axios";
 
 
-function Home() {
 
+function Home() {
+    const [oneReddit, setOneReddit] = useState([]);
     useEffect(() => {
-        async function getAllReddit() {
+        async function getOneReddit() {
 
             try {
 
                 const response = await axios.get("https://www.reddit.com/hot.json?limit=15")
-                console.log(response)
+                /*console.log(response.data.data.children[0])*/
+                /*setOneReddit(response)*/
+                setOneReddit(response.data.data.children) /*this is only for the 15 posts ! */
             } catch (e) {
-                console.log(e);
+                console.log(e)
             }
-
-
         }
 
-        getAllReddit()
+        getOneReddit()
 
-    }, []);
 
+    }, [])
 
 
     return (
         <>
+            <header className="outer-container">
+                <div className="inner-container">
+                <img src={Logo} id="logo-image" alt='reddit-logo'/>
+                <h1>Reddit</h1>
+                </div>
+            </header>
 
-            <img src={Logo} id="logo-image" alt='reddit-logo'/>
-            <h1>Reddit</h1>
+            <main className="outer-container">
+                <div className="inner-container">
+                    <h2>hottest posts</h2>
+                    <h4>on reddit right now</h4>
+                    <section className="reddit-posts">
+                        <article className="reddit-posts">
+                            <div className="reddit-post-list">
+                                {oneReddit.map((reddit)=>{
+                                    return (
+                                    <article key={reddit.data.id}>
+                                        <h3>{reddit.data.title}</h3>
+                                    </article>
 
+                                )
+                                })}
+                            </div>
+                        </article>
+                    </section>
+                </div>
+            </main>
         </>
-    );
-}
-
-export default Home;
+    )}          export default Home;
 
 
