270d180937a7bf9132cc04d50c0f38aab9663bb0
LuciaRyan <lcryan06@gmail.com>
2022-12-16 14:15:08 +0100

    started with build Subreddit page - just at start - has to be worked on

--- src/pages/subreddit/Subreddit.js
+++ src/pages/subreddit/Subreddit.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useParams} from "react-router-dom";
+
+import '../../../src/App'
+const Subreddit = () => {
 
 
-function Subreddit() {
+    const [redditDetails, setRedditDetails] = useState('')
+    const {subredditId} = useParams()
+
     return (
         <>
             <main className="subreddit-page">
-            What the hell is a subreddit ?!?!
+                <h3>This is the page for the subreddits</h3>
+                <span className="reddit-content">
+                    {
+
+
+                    }
+
+
+                </span>
+
+
             </main>
         </>
     );
