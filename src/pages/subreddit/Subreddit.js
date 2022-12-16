ffa3e7573ae90ff1d4be4933bd412d60f0c01999
LuciaRyan <lcryan06@gmail.com>
2022-12-16 17:12:25 +0100

    subreddit.js still faulty, cannot inject content on page...404

--- src/pages/subreddit/Subreddit.js
+++ src/pages/subreddit/Subreddit.js
@@ -1,27 +1,52 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
+import axios from "axios";
 
-import '../../../src/App'
 const Subreddit = () => {
 
 
-    const [redditDetails, setRedditDetails] = useState('')
-    const {subredditId} = useParams()
+    const [redditDetails, setRedditDetails] = useState({});
+    const {id} = useParams();
+
+    useEffect(() => {
+        async function getDetails() {
+
+            try {
+
+                const response = await axios.get(`https://www.reddit.com/r/${id}/about.json`)
+                setRedditDetails(response.data.data)
+
+            } catch (e) {
+                console.log(e)
+            }
+
+        }
+
+        if (id) {
+            getDetails()
+        }
+    }, [id])
+
 
     return (
+
+
         <>
-            <main className="subreddit-page">
-                <h3>This is the page for the subreddits</h3>
-                <span className="reddit-content">
-                    {
+
+            <header>
+                <h1>r/{id}</h1>
+                <h4>{}</h4>
+            </header>
+
+            <main className="subreddit-specs-page">
+                <section className="outer-container-specs">
+                    <div className="inner-container-specs">
 
 
-                    }
 
+                    </div>
 
-                </span>
-
-
+                </section>
             </main>
         </>
     );
